a064770d4f0bcb367fa09846b814bfaa4d42c7f4
Josue <ijosue.carreon@gmail.com>
2023-03-21 09:07:58 -0500

    api routes in progress

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+/* eslint-disable no-unused-vars */
+require("dotenv").config();
+
+const express = require("express");
+
+const cors = require("cors");
+
+const morgan = require('morgan');
+
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+const { 
+    createUser,
+    createActivity
+} = require("./db/index")
 
 // Setup your Middleware and API Router here
+
+app.use(morgan('dev'));
+
+app.use(express.json())
+
+const router = require('./api');
+app.use('/api', router);
+
+app.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+    next();
+});
+
 
 module.exports = app;
