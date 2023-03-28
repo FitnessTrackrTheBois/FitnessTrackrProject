e751fe17477eae43a000e98427fd858e1afde0a1
David Kapaku <dkapaku1@vt.edu>
2023-03-28 11:54:48 -0400

    please work im so tired

--- db/seed.js
+++ db/seed.js
@@ -3,7 +3,6 @@ DO NOT CHANGE THIS FILE
 */
 const client = require('./client');
 const { rebuildDB } = require('./seedData');
-const client = new client(process.env.DATABASE_URL);
 rebuildDB()
   .catch(console.error)
   .finally(() => client.end());
