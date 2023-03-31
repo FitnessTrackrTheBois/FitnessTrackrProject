65db570823ef34de3933ac8c7d186de71cf87b04
Josue <ijosue.carreon@gmail.com>
2023-03-30 23:36:25 -0500

    update

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 import { useNavigate } from "react-router-dom";
 
 // const DATABASE_URL = 'http://localhost:3000/api/';
-const DATABASE_URL = 'https://fitnesstracker-optw.onrender.com';
+const DATABASE_URL = 'https://fitnesstracker-optw.onrender.com/api';
 
 
 const Profile = (props) =>{
